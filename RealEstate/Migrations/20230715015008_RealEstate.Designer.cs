7afe5884ec41999710a031c05d000a7f33c4096e
shivn06 <shivnaidu06@gmail.com>
2023-07-15 22:22:03 +1200

    added table links in nav bar for easier access

--- RealEstate/Migrations/20230715015008_RealEstate.Designer.cs
+++ RealEstate/Migrations/20230715015008_RealEstate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealEstate.Areas.Identity.Data;
 
@@ -11,9 +12,11 @@ using RealEstate.Areas.Identity.Data;
 namespace RealEstate.Migrations
 {
     [DbContext(typeof(RealEstateContext))]
-    partial class RealEstateContextModelSnapshot : ModelSnapshot
+    [Migration("20230715015008_RealEstate")]
+    partial class RealEstate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
