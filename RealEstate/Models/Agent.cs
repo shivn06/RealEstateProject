fd013535a23d6357bc2e9386a1278fe53836ba62
shivn06 <shivnaidu06@gmail.com>
2023-10-19 00:25:05 +1300

    added test validations

--- RealEstate/Models/Agent.cs
+++ RealEstate/Models/Agent.cs
@@ -1,11 +1,33 @@
-﻿namespace RealEstate.Models
+﻿using RealEstate.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace RealEstate.Models
 {
     public class Agent
     {
+        [ScaffoldColumn(false)]
         public int AgentID { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        [DataType(DataType.Text)]
         public required string LastName { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        [DataType(DataType.Text)]
         public required string FirstName { get; set; }
-        public required string Phone { get; set; }
+
+        [Required]
+        [DisplayName("Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
         public required string Email { get; set; }
 
         public required ICollection<Client>Clients { get; set; }
