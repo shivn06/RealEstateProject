7321e728606ca8f723ba828e14ee684dd7434a92
shivn06 <shivnaidu06@gmail.com>
2023-10-24 22:34:09 +1300

    revised validation code for Agent model

--- RealEstate/Models/Agent.cs
+++ RealEstate/Models/Agent.cs
@@ -13,24 +13,24 @@ namespace RealEstate.Models
         [Required]
         [DisplayName("Last Name")]
         [DataType(DataType.Text)]
-        public required string LastName { get; set; }
+        public string LastName { get; set; }
 
         [Required]
         [DisplayName("First Name")]
         [DataType(DataType.Text)]
-        public required string FirstName { get; set; }
+        public string FirstName { get; set; }
 
         [Required]
         [DisplayName("Phone Number")]
         [DataType(DataType.PhoneNumber)]
         [Phone]
-        [RegularExpression("^[1-9]\\d{2}-\\d{3}-\\d{4}",ErrorMessage ="Please enter a valid Phone Number")]
+        [RegularExpression("^(((\\+?64\\s*[-\\.\\ ]?[3-9]|\\(?0[3-9]\\)?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?\\d{4})|((\\+?64\\s*[-\\.\\(\\ ]?2\\d{1,2}[-\\.\\)\\ ]?|\\(?02\\d{1}\\)?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?\\d{3,5})|((\\+?64\\s*[-\\.\\ ]?[-\\.\\(\\ ]?800[-\\.\\)\\ ]?|[-\\.\\(\\ ]?0800[-\\.\\)\\ ]?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?(\\d{2}|\\d{5})))|^$$", ErrorMessage ="Please enter a valid Phone Number")]
         public string Phone { get; set; }
 
         [Required]
         [DisplayName("Email")]
         [DataType(DataType.EmailAddress)]
-        public required string Email { get; set; }
+        public string Email { get; set; }
 
         public required ICollection<Client>Clients { get; set; }
         public required ICollection<Listing> Listings { get; set; }
--- RealEstate/Models/Client.cs
+++ RealEstate/Models/Client.cs
@@ -1,15 +1,41 @@
-﻿namespace RealEstate.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models
 {
     public class Client
     {
-        public required int ClientID { get; set; }
-        public required string LastName { get; set; }
-        public required string FirstName { get; set; }
-        public required string Email { get; set; }
-        public required string Phone { get; set; }
-        public required string Address { get; set; }
-        public required int PostalCode { get; set; }
+        public int ClientID { get; set; }
 
-        public required ICollection<Agent>Agents { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Phone Number")]
+        [RegularExpression("^(((\\+?64\\s*[-\\.\\ ]?[3-9]|\\(?0[3-9]\\)?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?\\d{4})|((\\+?64\\s*[-\\.\\(\\ ]?2\\d{1,2}[-\\.\\)\\ ]?|\\(?02\\d{1}\\)?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?\\d{3,5})|((\\+?64\\s*[-\\.\\ ]?[-\\.\\(\\ ]?800[-\\.\\)\\ ]?|[-\\.\\(\\ ]?0800[-\\.\\)\\ ]?)\\s*[-\\.\\ ]?\\d{3}\\s*[-\\.\\ ]?(\\d{2}|\\d{5})))|^$$", ErrorMessage = "Please enter a valid Phone Number")]
+        public string Phone { get; set; }
+
+        [Required]
+        [DisplayName("Address")]
+        [DataType(DataType.Text)]
+        public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.PostalCode)]
+        public int PostalCode { get; set; }
+
+        public ICollection<Agent>Agents { get; set; }
     }
 }
